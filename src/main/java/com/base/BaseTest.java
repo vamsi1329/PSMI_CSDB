dfd379d4be9f4fbf7b1a2e60d37be6696acc150f
Vamsi Bevara <CE515@compassitesinc.com>
2019-06-26 17:36:57 +0530

    Tool form page update 1

--- src/main/java/com/base/BaseTest.java
+++ src/main/java/com/base/BaseTest.java
@@ -3,8 +3,10 @@ package com.base;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class BaseTest {
 
@@ -35,5 +37,10 @@ public class BaseTest {
 		driver.get(url);
 		driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	public void dropDownSelection(WebElement element, String dropDownText){
+		Select s = new Select(element);
+		s.selectByVisibleText(dropDownText);
 	}
 }
--- src/main/java/com/pages/ToolFormPage.java
+++ src/main/java/com/pages/ToolFormPage.java
@@ -1,7 +1,30 @@
 package com.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import com.base.BaseTest;
 
 public class ToolFormPage extends BaseTest {
 
+	@FindBy(id = "li-toolform")
+	WebElement ToolFormTab;
+
+	@FindBy(id = "Opportunity_CompanyId")
+	WebElement EGCCompanyDD;
+
+	@FindBy(id = "Opportunity_CustomerId")
+	WebElement CustomerDD;
+
+	@FindBy(id = "Opportunity_PlantId")
+	WebElement PlanDD;
+
+	@FindBy(id = "Opportunity_DepartmentId")
+	WebElement DepartmentDD;
+
+	@FindBy(id = "Opportunity_MachineOrStationId")
+	WebElement StationOrMachineDD;
+
+	@FindBy(id = "Opportunity_TransactionalCurrencyId")
+	WebElement CurrencyDD;
 }
